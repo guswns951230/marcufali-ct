51dac27b90dccad357e569367b6d2d09e5dbd9b6
Kang <rkdguswns0803@gmail.com>
2022-05-29 09:28:58 +0900

    store section css, modal 추가

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
 'use strict'
 
+// modal
+const modal = document.querySelector('.store__modal');
+const imgs = document.querySelectorAll('.store__img')
+const modalImg = document.querySelector('.store__modal-img');
+const span = document.querySelector('.store__modal-close');
+
+function modalDisplay(text) {
+  modal.style.display = text;
+}
+
+imgs.forEach(img => {
+  img.addEventListener('click', () => {
+    modalDisplay('block');
+    modalImg.src = img.src;
+  });
+});
+
+span.addEventListener('click', () => {
+  modalDisplay('none');
+});
+
+modal.addEventListener('click', () => {
+  modalDisplay('none');
+});
+
 // map api
 let marcufali = new naver.maps.LatLng(35.1786420, 129.1271746);
 
